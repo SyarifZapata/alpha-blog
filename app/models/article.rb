faf45a4b64fc826bcede6a2a6cbbf40214ba59ee
syarifmathis <syarif.mathis@gmail.com>
2017-02-15 20:58:38 +0100

    Create article Table and put validation

---
+++ app/models/article.rb
@@ -0,0 +1,10 @@
+#it gave the columns its getter and setter
+#Validates means it can only be saved if title is not empty
+#Or we can Validates the data with any other condition.
+
+class Article < ActiveRecord::Base
+  validates :title, presence: true, length: {minimum: 3, maximum: 50}
+  validates :description, presence: true, length: {minimum: 10, maximum: 300}
+
+end
+
