d6455f26ba9b79e795b68675294856b4b99d7a39
syarifmathis <syarif.mathis@gmail.com>
2017-02-16 10:13:37 +0100

    add edit, update, show and index listing

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,6 +4,23 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Article was successfully updated"
+      redirect_to article_path(@article)
+    else
+      render 'update'
+    end
+  end
+
+  def index
+    @articles = Article.all
+  end
 
 
   def create
@@ -15,11 +32,9 @@ class ArticlesController < ApplicationController
     else
       render 'new'
     end
-
-
   end
 
-  
+
  def show
    @article = Article.find(params[:id])
  end
