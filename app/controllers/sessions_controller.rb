d89547be6ab7f6197ac23b50d2d4504bbb3980ae
syarifmathis <syarif.mathis@gmail.com>
2017-02-19 20:00:48 +0100

    debug login session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,7 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       session[:user_id] = user.id
       flash[:success] = "You have successfully logged in"
-      redirect_to users_path(user)
+      redirect_to user_path(user)
     else
       flash.now[:danger] = "There was something wrong with your login information"
       render 'new'
@@ -17,7 +17,9 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-
+    session[:user_id] = nil
+    flash[:success] = "you have logged out"
+    redirect_to root_path
   end
 
 end
