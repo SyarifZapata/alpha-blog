5bf1bc669aee5c7967ab14963e42cfa0c47eeb30
syarifmathis <syarif.mathis@gmail.com>
2017-02-20 23:42:39 +0100

    signup user logged in automaticaly

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,17 +11,18 @@ class UsersController < ApplicationController
   end
 
   def create
-    #@user = User.new(user_params)
+    @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:success] = "Welcome to the alpha blog #{@user.username}"
-      redirect_to articles_path
+      redirect_to user_path(@user)
     else
       render 'new'
     end
   end
 
   def edit
-
+z
   end
 
   def update
